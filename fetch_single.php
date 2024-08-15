c88e4377d1ad3f5b5fe5c87047437412a90ae464
NandhaKIT <nandha@kaashivinfotech.in>
2024-08-15 16:45:46 +0530

    Update fetch_single.php
    changed in fetch_single.php

--- fetch_single.php
+++ fetch_single.php
@@ -1,20 +1,24 @@
 <?php
 include('db.php');
 include('function.php');
-if(isset($_POST["member_id"]))
+if(isset($_POST["t_id"]))
 {
     $output = array();
     $statement = $connection->prepare(
-        "SELECT * FROM member WHERE id = '".$_POST["member_id"]."' LIMIT 1"
+        "SELECT * FROM tbl_trainees WHERE id = '".$_POST["t_id"]."' LIMIT 1"
     );
     $statement->execute();
     $result = $statement->fetchAll();
     foreach($result as $row)
     {
-        $output["id"] = $row["id"];
-        $output["name"] = $row["name"];
-        $output["email"] = $row["email"];
-        $output["phone"] = $row["phone"];
+        $output["t_kid"] = $row["t_kid"];
+        $output["t_name"] = $row["t_name"];
+        $output["t_email"] = $row["t_email"];
+        $output["t_pass"] = $row["t_pass"];
+        $output["t_status"] = $row["t_status"];
+        $output["t_tech"] = $row["t_tech"];
+        $output["t_avail"] = $row["avail"];
+        
     }
     echo json_encode($output);
 }
