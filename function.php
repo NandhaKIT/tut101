85b27e800067d1cb57dff63aff1bd75279ac755a
NandhaKIT <nandha@kaashivinfotech.in>
2024-08-15 17:05:34 +0530

    updated insertupdated.php
    updated insertupdated.php

--- function.php
+++ function.php
@@ -2,7 +2,7 @@
 function get_total_all_records()
 {
     include('db.php');
-    $statement = $connection->prepare("SELECT * FROM member");
+    $statement = $connection->prepare("SELECT * FROM trainees");
     $statement->execute();
     $result = $statement->fetchAll();
     return $statement->rowCount();
